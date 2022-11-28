97d9c4b1fc490e07c958bff449b1e38d1089c20e
Ilya Markov <imarkovkin@gmail.com>
2022-11-28 23:07:13 +0300

    Добавьте файлы проекта.

---
+++ Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+
+{
+    class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            Product[] products = new Product[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите код товара");
+                int code = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите название товара");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите стоимость товара");
+                double price = Convert.ToInt32(Console.ReadLine());
+
+                products[i] = new Product() { Code = code, Name = name, Price = price };
+            }
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+
+            string jsonString = JsonSerializer.Serialize(products, options);
+
+            using (StreamWriter sw = new StreamWriter("../../../Products.json"))
+            {
+                sw.WriteLine(jsonString);
+            }
+        }
+    }
+}
